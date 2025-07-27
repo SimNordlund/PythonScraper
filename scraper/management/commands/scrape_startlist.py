76282c327da3aab703de6ac6fa00dc9b494c2775
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-07-27 20:47:38 +0200

    ids

--- scraper/management/commands/scrape_startlist.py
+++ scraper/management/commands/scrape_startlist.py
@@ -113,8 +113,8 @@ async def scrape_startlist(url: str) -> List[StartRow]:
 
 
 class Command(BaseCommand):
-    START_ID = 609_988
-    END_ID   = 610_050
+    START_ID = 610_020
+    END_ID   = 610_100
     help = "Scrape hard-coded ts-ID range into Startlista"
 
     def handle(self, *args, **kwargs):
