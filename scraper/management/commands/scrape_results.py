f117a58ed84e187ba57eaebd4c570fd5e0bd1bfa
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-09-30 19:23:45 +0200

    scraping data from 2024 until today in order to move vb project into cloud

--- scraper/management/commands/scrape_results.py
+++ scraper/management/commands/scrape_results.py
@@ -149,7 +149,7 @@ async def scrape_page(url: str) -> List[Row]:
 class Command(BaseCommand):
     help = "Scrape fixed ID range 609766 → 609963 into Resultat"
 
-    START_ID = 610_180
+    START_ID = 605_104
     END_ID   = 610_235
 
     def handle(self, *args, **opts):
