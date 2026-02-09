ddfc49e7d99b7f1c286ed875992b610f2b3127ab
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2026-02-09 20:10:29 +0100

    url

--- scraper/management/commands/scrape_results.py
+++ scraper/management/commands/scrape_results.py
@@ -607,8 +607,8 @@ async def run_range(start_id: int, end_id: int) -> int:
 class Command(BaseCommand):
     help = "Scrape hard-coded ts-ID range into Result"
 
-    START_ID = 616_080
-    END_ID = 616_100
+    START_ID = 616_109
+    END_ID = 616_125
     
     #START_ID = 600_569
     #END_ID = 601_432
